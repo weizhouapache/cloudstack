482b13ba4ef3dffad124c4c079ee97c17b2bc8df
Wei Zhou <weizhou@apache.org>
2025-10-02 20:49:25 +0200

    VNF: create BaseVnfProvider

--- core/src/main/java/org/apache/cloudstack/vnf/BaseVnfProvider.java
+++ core/src/main/java/org/apache/cloudstack/vnf/BaseVnfProvider.java
@@ -18,5 +18,28 @@
  */
 package org.apache.cloudstack.vnf;
 
-public class DummyVnfProvider extends BaseVnfProvider {
+import com.cloud.utils.component.AdapterBase;
+
+public class BaseVnfProvider extends AdapterBase implements VnfProvider {
+
+    protected String name;
+    protected String description;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
